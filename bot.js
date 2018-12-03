325e98bf269c68f7fd071d40ca388ff6619ce952
Edude42 <34699884+Edude42@users.noreply.github.com>
2018-12-02 21:10:09 -0500

    Successful "Ping" command

--- bot.js
+++ bot.js
@@ -12,28 +12,28 @@ var bot = new Discord.Client({
    token: auth.token,
    autorun: true
 });
+
+// Display boot message
 bot.on('ready', function (evt) {
     logger.info('Connected');
-    logger.info('Logged in as: ');
+    logger.info('Bot startup successful');
     logger.info(bot.username + ' - (' + bot.id + ')');
 });
 bot.on('message', function (user, userID, channelID, message, evt) {
-    // Our bot needs to know if it will execute a command
-    // It will listen for messages that will start with `!`
-    if (message.substring(0, 1) == '!') {
+    // Bot prefix: "]"
+    if (message.substring(0, 1) == ']') {
         var args = message.substring(1).split(' ');
         var cmd = args[0];
        
         args = args.splice(1);
         switch(cmd) {
-            // :ping
+            // ]ping
             case 'ping':
                 bot.sendMessage({
                     to: channelID,
                     message: 'Pong!'
                 });
             break;
-            // Just add any case commands if you want to..
          }
      }
 });
