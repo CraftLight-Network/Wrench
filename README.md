1575b174046550ba977923ecbbb0cfa5de7911b4
Edude42 <34699884+Edude42@users.noreply.github.com>
2018-12-03 21:59:36 -0500

    Added slots and common-tags

--- README.md
+++ README.md
@@ -5,6 +5,8 @@ Please do not take this bot and claim it as your own.
 You are free to use this on your own server, but there is no documentation.
 
 To use this bot, do
-npm i discord.js && npm i discord.js-commando && npm i -S sqlite
+1. npm i discord.js
+2. npm i discord.js-commando
+3. npm i sqlite
 
 Enjoy!
---
+++ commands/fun/slots.js
@@ -0,0 +1,98 @@
+﻿const { Command } = require('discord.js-commando');
+const { stripIndents } = require('common-tags');
+
+const combinations = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 4, 8], [2, 4, 6]];
+const reels = [
+	['🍒', '💰', '⭐', '🎲', '💎', '❤', '⚜', '🔅', '🎉'],
+	['💎', '🔅', '❤', '🍒', '🎉', '⚜', '🎲', '⭐', '💰'],
+	['❤', '🎲', '💎', '⭐', '⚜', '🍒', '💰', '🎉', '🔅']
+];
+
+const values = {
+	'💎': 500,
+	'⚜': 400,
+	'💰': 400,
+	'❤': 300,
+	'⭐': 300,
+	'🎲': 250,
+	'🔅': 250,
+	'🎉': 250,
+	'🍒': 250
+};
+
+module.exports = class SlotMachineCommand extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'slots',
+			aliases: ['slot', 'slot-machine'],
+			group: 'fun',
+			memberName: 'slots',
+			description: 'Play the slots.',
+			examples: ['slots'],
+			throttling: {
+				usages: 1,
+				duration: 5
+			},
+		});
+	}
+
+	async run(msg, { coins }) {
+
+		const roll = this.generateRoll();
+		let winnings = 0;
+
+		combinations.forEach(combo => {
+			if (roll[combo[0]] === roll[combo[1]] && roll[combo[1]] === roll[combo[2]]) {
+				winnings += values[roll[combo[0]]];
+			}
+		});
+
+		if (winnings === 0) {
+			return msg.embed({
+				color: 0xBE1931,
+				description: stripIndents`
+					**${msg.member.displayName}, you rolled:**
+
+					${this.showRoll(roll)}
+
+					**You lost!**
+					Better luck next time!
+				`
+			});
+		}
+
+		Currency.addBalance(msg.author.id, coins * winnings);
+
+		return msg.embed({
+			color: 0x5C913B,
+			description: stripIndents`
+				**${msg.member.displayName}, you rolled:**
+
+				${this.showRoll(roll)}
+
+				**Congratulations!**
+				You won ${Currency.convert(coins * winnings)}!
+			`
+		});
+	}
+
+	showRoll(roll) {
+		return stripIndents`
+			${roll[0]}ー${roll[1]}ー${roll[2]}
+			${roll[3]}ー${roll[4]}ー${roll[5]}
+			${roll[6]}ー${roll[7]}ー${roll[8]}
+		`;
+	}
+
+	generateRoll() {
+		const roll = [];
+		reels.forEach((reel, index) => {
+			const rand = Math.floor(Math.random() * reel.length);
+			roll[index] = rand === 0 ? reel[reel.length - 1] : reel[rand - 1];
+			roll[index + 3] = reel[rand];
+			roll[index + 6] = rand === reel.length - 1 ? reel[0] : reel[rand + 1];
+		});
+
+		return roll;
+	}
+};
