a69b41202fa2758c667b82ba739c669e925100c8
Edude42 <egonnelly@gmail.com>
2019-08-20 11:32:47 -0400

    Re-wrote server-info

--- commands/helpful/server-info.js
+++ commands/helpful/server-info.js
@@ -1,5 +1,6 @@
 ﻿const { Command } = require('discord.js-commando');
 const moment = require('moment');
+const { RichEmbed } = require('discord.js');
 const { stripIndents } = require('common-tags');
 
 const humanLevels = {
@@ -27,40 +28,28 @@ module.exports = class serverInfoCommand extends Command {
 		});
 	}
 	run(msg) {
-		return msg.embed({
-			color: 4021408,
-			description: `Info of **${msg.guild.name}** (ID: ${msg.guild.id})`,
-			fields: [
-				{
-					name: '➠ Channels',
-					/* eslint-disable max-len */
-					value: stripIndents`
-						• ${msg.guild.channels.filter(ch => ch.type === 'text').size} Text, ${msg.guild.channels.filter(ch => ch.type === 'voice').size} Voice
-						• AFK: ${msg.guild.afkChannelID ? `<#${msg.guild.afkChannelID}> after ${msg.guild.afkTimeout / 60}min` : 'None.'}
-					`,
-					/* eslint-enable max-len */
-					inline: true
-				},
-				{
-					name: '➠ Member',
-					value: stripIndents`
-						• ${msg.guild.memberCount} members
-						• Owner: ${msg.guild.owner.user.tag}
-						(ID: ${msg.guild.ownerID})
-					`,
-					inline: true
-				},
-				{
-					name: '➠ Other',
-					value: stripIndents`
-						• Roles: ${msg.guild.roles.size}
-						• Region: ${msg.guild.region}
-						• Created at: ${moment.utc(msg.guild.createdAt).format('dddd, MMMM Do YYYY, HH:mm:ss')}
-						• Verification Level: ${humanLevels[msg.guild.verificationLevel]}
-					`
-				}
-			],
-			thumbnail: { url: msg.guild.iconURL }
-		});
+		
+		const embed = new RichEmbed()
+		.setDescription(`**ID: ${msg.guild.id}**`)
+		.setAuthor(`Server Info: ${msg.guild.name}`)
+		.setThumbnail(msg.guild.iconURL)
+		.addField('**» Channels**', stripIndents`
+			• ${msg.guild.channels.filter(ch => ch.type === 'text').size} Text, ${msg.guild.channels.filter(ch => ch.type === 'voice').size} Voice
+			• AFK: ${msg.guild.afkChannelID ? `<#${msg.guild.afkChannelID}> after ${msg.guild.afkTimeout / 60}min` : 'None.'}
+		`, true)
+		.addField('**» Members**', stripIndents`
+			• ${msg.guild.memberCount} members
+			• Owner: ${msg.guild.owner.user.tag}
+			  (${msg.guild.ownerID})
+		`, true)
+		.addField('**» Misc.**', stripIndents`
+			• Roles: ${msg.guild.roles.size}
+			• Region: ${msg.guild.region}
+			• Created on: ${moment.utc(msg.guild.createdAt).format('dddd, MMMM Do YYYY, h:mm:ss A (M/D/YY)')}
+			• Verification Level: ${humanLevels[msg.guild.verificationLevel]}
+		`, true)
+		.setColor(0x2F5EA3)
+		.setTimestamp();
+		return msg.say(embed);
 	}
 };
