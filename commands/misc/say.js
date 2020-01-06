6a8c3216c406ad5214bc73e35bf2dbd99899b6e7
Edude42 <me@edude42.dev>
2020-01-05 20:39:58 -0500

    Yay! Say command!

---
+++ commands/misc/say.js
@@ -0,0 +1,37 @@
+const { Command } = require("discord.js-commando");
+const { stripIndents } = require('common-tags');
+const config = require("../../config.json");
+
+module.exports = class sayCommand extends Command {
+	constructor(client) {
+		super(client, {
+			"name": "say",
+			"memberName": "say",
+			"group": "misc",
+			"description": "Make the bot say stuff.",
+			"details": stripIndents`
+				Run \`${config.prefix}say [args]\` to make the bot say anything.
+				**Notes:**
+				[args]: Required, what the bot will say.
+				Arguments must be under 400 characters.
+			`,
+			"args": [
+				{
+					"key": "toSay",
+					"prompt": "What would you like me to say?",
+					"default": "",
+					"type": "string",
+					"validate": arg => {
+						if (arg.length < 400 && arg.trim().length > 0) return true;
+						return "please use under 400 characters!";
+					}
+				}
+			]
+		});
+	}
+
+	run(message, { toSay }) {
+		if (message.guild) {message.delete()};
+		return message.say(toSay);
+	}
+};
