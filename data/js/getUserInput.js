26f51fcd1fb7f399151f1214fc3034c2caa0c9bd
Edude42 <me@edude42.dev>
2020-02-25 19:58:03 -0500

    Make cancelling and cancel message optional

--- data/js/getUserInput.js
+++ data/js/getUserInput.js
@@ -4,15 +4,19 @@ const { stripIndents } = require("common-tags");
 module.exports = async function (message, options) {
 	// Default options if not defined
 	if (!options.question) options.question = "What would you like to do?";
+	if (options.cancel === undefined) options.cancel = true;
 	if (!options.timeout) options.timeout = 30;
 	if (!options.validate) options.validate = false;
 
 	let result = options.variable;
 	let exit;
 	while (!result && !exit) {
+		let cancel = "";
+		if (options.cancel) cancel = `Respond with \`cancel\` to cancel the command. The command will automatically be cancelled in ${options.timeout} seconds.`;
+
 		message.reply(stripIndents`
 			${options.question}
-			Respond with \`cancel\` to cancel the command. The command will automatically be cancelled in ${options.timeout} seconds.
+			${cancel}
 		`);
 
 		// Take user input
@@ -23,10 +27,10 @@ module.exports = async function (message, options) {
 
 		// Set result to input
 		result.find(i => {result = i.content});
-		if (result === "cancel") break;
+		if (options.cancel) if (result === "cancel") break;
 
 		// Validate input
-		if (!options.validate) return;
+		if (!options.validate) break;
 		if (!options.validate.array.includes(result)) {
 			result = "";
 			return message.reply(`Invalid ${options.validate.name}.`);
