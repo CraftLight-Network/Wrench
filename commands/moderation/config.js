0fa9e139d1e40932572e122f8951d5fb3e94b683
Encode42 <me@edude42.dev>
2020-05-14 00:37:46 -0400

    Config editor

---
+++ commands/moderation/config.js
@@ -0,0 +1,114 @@
+// Define and require modules
+const configHandler = require("../../data/js/configHandler");
+const userInput = require("../../data/js/util").getUserInput;
+const checkRole = require("../../data/js/util").checkRole;
+const { guildConfig } = require("../../data/js/enmap");
+const { Command } = require("discord.js-commando");
+const embed = require("../../data/js/util").embed;
+const { stripIndents } = require("common-tags");
+const config = require("../../config");
+const safeEval = require("safe-eval");
+
+const actions = ["view", "set", "add", "remove", "reset"];
+
+module.exports = class configCommand extends Command {
+	constructor(client) {
+		super(client, {
+			"name": "config",
+			"memberName": "config",
+			"aliases": ["conf"],
+			"group": "moderation",
+			"description": "Edit the server configuration.",
+			"details": stripIndents`
+				Run \`${config.prefix.commands}tag <action> (property) (value)\` to use commands.
+				**Notes:**
+				<action>: Required, what to do.
+				(name): Required depending on action, what property to take action on.
+				(content): Required depending on action, what to set the value of property to.
+
+				Actions: \`${actions.join("`, `")}\`
+			`,
+			"args": [
+				{
+					"key": "action",
+					"prompt": "What would you like to do?",
+					"type": "string",
+					"oneOf": actions
+				},
+				{
+					"key": "property",
+					"prompt": "",
+					"default": "",
+					"type": "string"
+				},
+				{
+					"key": "value",
+					"prompt": "",
+					"default": "",
+					"type": "string"
+				}
+			],
+			"guildOnly": true,
+			"clientPermissions": ["SEND_MESSAGES", "EMBED_LINKS"],
+			"throttling": {
+				"usages": 2,
+				"duration": 5
+			}
+		});
+	}
+
+	async run(message, { action, property, value }) {
+		const guildConfig = await configHandler.getConfig(message.guild.id);
+
+		if (!checkRole(guildConfig.automod.modRoleIDs, message)) return message.reply("You do not have permission to use this command.");
+
+
+		if (action === "view") {
+			const embedMessage = {
+				"title": `${message.guild.name}'s config:`,
+				"description": `
+\`\`\`json
+${JSON.stringify(guildConfig, null, 2)}
+\`\`\`
+				`
+			};
+			return message.channel.send(embed(embedMessage, message));
+		}
+
+		if (action === "reset") {
+			configHandler.reset(message.guild.id);
+			return message.reply("Successfully reset the config.");
+		}
+
+		if (!checkExists()) return message.reply("That config property does not exist.");
+
+		if (action === "set") {
+			configHandler.setConfig(message.guild.id, property, value);
+			return message.reply(`Set ${property} to ${value}.`);
+		}
+
+		if (action === "add") {
+			if (!isArray()) return message.reply("That config property is not an array. Use `set` instead.");
+
+			configHandler.addConfig(message.guild.id, property, value);
+			return message.reply(`Added ${value} to ${property}.`);
+		}
+
+		if (action === "remove") {
+			if (!isArray()) return message.reply("That config property is not an array. Use `set` instead.");
+
+			configHandler.removeConfig(message.guild.id, property, value);
+			return message.reply(`Removed ${value} from ${property}.`);
+		}
+
+		function checkExists() {
+			if (safeEval(`guildConfig.${property}`, { "guildConfig": guildConfig })) return true;
+			return false;
+		}
+
+		function isArray() {
+			if (safeEval(`guildConfig.${property}`, { "guildConfig": guildConfig }) instanceof Array) return true;
+			return false;
+		}
+	}
+};
--- data/js/configHandler.js
+++ data/js/configHandler.js
@@ -1,7 +1,9 @@
-// Get logger
+// Define and require modules
 const defaultConfig = require("../json/default");
 const { guildConfig } = require("./enmap");
+const safeEval = require("safe-eval");
 const merge = require("merge-json");
+const _ = require("lodash");
 
 // Create the config
 module.exports.ensure = (guild) => {
@@ -11,11 +13,32 @@ module.exports.ensure = (guild) => {
 // Get the config
 module.exports.getConfig = async (guild) => {
 	this.ensure(guild);
-	return merge.merge(defaultConfig, await guildConfig.get(guild));
+	return merge.merge(_.cloneDeep(defaultConfig), await guildConfig.get(guild));
 };
 
 // Set the config
 module.exports.setConfig = async (guild, property, value) => {
+	guildConfig.evict(`${guild}.${property}`);
+
+	if (!(safeEval(`config.${property}`, { "config": await this.getConfig(guild) }) instanceof Array)) return guildConfig.setProp(guild, property, value);
+	else return guildConfig.setProp(guild, property, [value]);
+};
+
+// Add to the config
+module.exports.addConfig = async (guild, property, value) => {
 	guildConfig.fetchEverything();
-	guildConfig.set(guild, value, property);
+
+	if (!guildConfig.get(guild, property)) return guildConfig.setProp(guild, property, [value]);
+	return guildConfig.pushIn(guild, property, value, true);
+};
+
+// Remove from the config
+module.exports.removeConfig = async (guild, property, value) => {
+	return guildConfig.removeFrom(guild, property, value);
+};
+
+// Reset the config
+module.exports.reset = (guild) => {
+	guildConfig.delete(guild);
+	return this.ensure(guild);
 };
