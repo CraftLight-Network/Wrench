9b311cdff56c948bd1da91df4e9c0b072725d2bf
Encode42 <me@encode42.dev>
2021-04-28 10:42:42 -0400

    Slightly faster random

--- src/main/java/org/craftlight/wrench/commands/utility/RandomCommand.java
+++ src/main/java/org/craftlight/wrench/commands/utility/RandomCommand.java
@@ -6,6 +6,7 @@ import com.github.kaktushose.jda.commands.entities.CommandEvent;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.util.SplittableRandom;
 
 @CommandController
 public class RandomCommand {
@@ -15,7 +16,8 @@ public class RandomCommand {
         int decimals = Math.max(min.scale(), max.scale());
 
         // Generate the random number
-        BigDecimal number = min.add(BigDecimal.valueOf(Math.random()).multiply(max.subtract(min)));
+        SplittableRandom random = new SplittableRandom();
+        BigDecimal number = min.add(BigDecimal.valueOf(random.nextDouble()).multiply(max.subtract(min)));
 
         // Reply
         event.reply("I have generated the number " + number.setScale(decimals, RoundingMode.HALF_UP));
